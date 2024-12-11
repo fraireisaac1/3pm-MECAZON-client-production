3f65f5e4c92e20de57c4db966f70ad92026b21ef
Myles Marshall <mmarsh319@west-mec.org>
2024-12-11 16:14:55 -0700

    Navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,18 @@ import styles from "../styles/Navbar.module.css";
 export default function Navbar() {
   return (
     <nav className={styles.nav}>
-      <Link to="/">Home</Link>
-      <Search></Search>
-      <Link to="/groceries">Groceries</Link>
+      <div className="nav-col">
+        <img src="https://www.shareicon.net/data/512x512/2016/08/01/640375_smile_512x512.png" alt="mecazon logo" height="25px" width="25px" />
+        <Link to="/">MECAZON</Link>
+      </div>
+
+      <div className="nav-col">
+        <Search></Search>
+      </div>
+
+      <div className="nav-col">
+        <Link to="/groceries">Sign in</Link>
+      </div>
     </nav>
   );
 }
