f82ad67571abdbdaa4364b53a306d2f7e751eab5
EMcNaughton-WestMec <emcnau058@west-mec.org>
2024-12-11 16:01:04 -0700

    Removed previous CSS styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import styles from "../styles/Navbar.module.css";
 export default function Navbar() {
   return (
     <nav className={styles.nav}>
-      <Link to="/">Home</Link>
+      <Link to="/">MECHAZON</Link>
       <Search></Search>
       <Link to="/groceries">Groceries</Link>
     </nav>
