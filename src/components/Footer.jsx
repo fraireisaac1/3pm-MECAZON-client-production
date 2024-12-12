625095239df031a075aa2b7a0c37120792bffa8d
EMcNaughton-WestMec <emcnau058@west-mec.org>
2024-12-11 17:27:47 -0700

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,28 @@
 export default function Footer() {
     return (
-        <>
-            <h1>--- Footer ---</h1>
-        </>
+        <row>
+            <column>
+                <h1>Help</h1>
+                <input type="email" name="" id="" placeholder="Email" />
+                <button>Submit</button>
+                <h2>Contact us</h2>
+            </column>
+            <column>
+                <h1>Socials</h1>
+            </column>
+            <column>
+                <row>
+                    <column>
+                        <h1>Values</h1>
+                    </column>
+                    <column>
+                        <h1>Account</h1>
+                    </column>
+                </row>
+                <row>
+                    <h3>2024 - MECAZON - All rights reserved</h3>
+                </row>
+            </column>
+        </row>
     );
 }
