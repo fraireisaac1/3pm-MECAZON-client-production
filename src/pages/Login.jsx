b47906acd0e192af15120d3228f6ead5a490bdff
EMcNaughton-WestMec <142362842+EMcNaughton-WestMec@users.noreply.github.com>
2024-12-12 04:53:52 +0000

    Login Styling

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,19 @@
 // basic skeleton for login
+import styles from "../styles/Login.module.css";
 
 export default function Login() {
     return (
-        <div>
-            <h1>Login Form</h1>
+        <div className={styles.Form}>
+            <div className={styles.Brand}>
+                <img className={styles.Logo} src="/MECAZON_logo.svg" alt="MECAZON LOGO" /> MECAZON
+            </div>
+            <h1 className={styles.Header}>Log in</h1>
             <form action="">
-                <label htmlFor="email">Email</label> <br />
-                <input type="email" id="email" name="email" required /> <br /> <br />
+                <label className={styles.Label} htmlFor="email">Email Address:</label> <br />
+                <input className={styles.Input} type="email" id="email" name="email" required /> <br /> <br />
 
-                <label htmlFor="password">Password</label> <br />
-                <input type="password" id="password" name="password" pattern="" required /> <br /> <br />
+                <label className={styles.Label} htmlFor="password">Password</label> <br />
+                <input className={styles.Input} type="password" id="password" name="password" pattern="" required /> <br /> <br />
 
                 <button type="submit">Login</button>
             </form>
