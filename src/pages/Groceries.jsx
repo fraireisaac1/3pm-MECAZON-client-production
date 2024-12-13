ebdff0cb6582964e68b51c7558b3b85c58b293b7
LazyKotaa <mylesmarshall06@gmail.com>
2024-12-12 22:50:06 -0700

    search bar filters properly (better than before)

--- src/pages/Groceries.jsx
+++ src/pages/Groceries.jsx
@@ -7,15 +7,16 @@ export default function Groceries({searchValue}) {
   const [groceries, setGroceries] = useState([]);
   const resultsTextRef = useRef(null);
 
-  useEffect(() => {
-    async function fetchGroceries() {
-      try {
-        const response = await axios.get("/dummy-data/groceries.json");
-        setGroceries(response.data);
-      } catch (err) {
-        console.error("something went wrong fetching groceries", err);
-      }
+  async function fetchGroceries() {
+    try {
+      const response = await axios.get("/dummy-data/groceries.json");
+      setGroceries(response.data);
+    } catch (err) {
+      console.error("something went wrong fetching groceries", err);
     }
+  }
+
+  useEffect(() => {
     fetchGroceries();
   }, []);
 
@@ -35,18 +36,26 @@ export default function Groceries({searchValue}) {
   },[productIndex])
 
   useEffect(() => {
-    if (searchValue) {
-      const results = [...groceries].filter(item => {
-          return Object.values(item).some(value => value.toString().toLowerCase().includes(searchValue.toLowerCase()));
-      });
+    async function renderSearchResults() {
+      if (searchValue) {
+        const response = await axios.get("/dummy-data/groceries.json");
 
-      setGroceries(results);
+        const results = response.data.filter(item => {
+          return Object.values(item).some(value => value.toString().toLowerCase().includes(searchValue.toLowerCase()));
+        });
+
+        setGroceries(results);
+      } else if (searchValue === "") {
+        fetchGroceries();
+      }
     }
+
+    renderSearchResults();
   }, [searchValue])
 
   return (
     <div className={styles.background}>
-      <h1 ref={resultsTextRef} className={styles.text}>Results for {searchValue}</h1>
+      <h1 ref={resultsTextRef} className={styles.text}>Results for "{searchValue}"</h1>
         <div>
           <GroceryList items={groceries} />
         </div>
