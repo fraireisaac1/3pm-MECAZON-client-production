f5ad6d481f20e263e07b6352e2c918d942a83fc7
Myles Marshall <mmarsh319@west-mec.org>
2024-12-13 16:44:05 -0700

    a

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,14 @@
-export default function Modal() {
+import styles from "../styles/Modal.module.css";
+import { useEffect } from "react";
+
+export default function Modal({data}) {
+    useEffect(() => {
+        console.log(data);
+    }, [data]);
+
     return (
-        <div>
-            
-        </div>
+        <dialog>
+            <h1>asdkjaskdasd</h1>
+        </dialog>
     );
 }
