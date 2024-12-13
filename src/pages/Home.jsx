ea28f4b24658dcfd70c19c933f9cbb00efcdeb14
EMcNaughton-WestMec <142362842+EMcNaughton-WestMec@users.noreply.github.com>
2024-12-13 06:16:34 +0000

    update or something

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import styles from "../styles/Home.module.css";
 import Carousel from "../components/Carousel";
 // import axios from "axios";
 
@@ -18,7 +19,12 @@ export default function Home() {
 
   return <>
     <Carousel count={Math.ceil(self.innerWidth/200)<10?Math.ceil(self.innerWidth/200):10}/>
-    <Carousel count={Math.ceil(self.innerWidth/200)<10?Math.ceil(self.innerWidth/200):10}/>
-    <Carousel count={Math.ceil(self.innerWidth/200)<10?Math.ceil(self.innerWidth/200):10}/>
+    <div className={styles.banner}>
+      <img className={styles.image} src="https://media-public.canva.com/ADwTE/MAGHDfADwTE/1/s.png" alt="Black Man" draggable="false"/>
+      <div className={styles.column}>
+        <h1 className={styles.title}>Business solutions</h1>
+        <p className={styles.text}>Slide for Von dotn slime ur homye. Praesent dui leo, euismod et purus eu, iaculis luctus mauris. Cras a dolor eget nisi aliquet pulvinar eu ut elit. Suspendisse dictum magna quis metus pulvin.</p>
+      </div>
+    </div>
   </>;
 }
