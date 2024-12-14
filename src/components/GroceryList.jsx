7cebd223cb5b2b50f29d8c09aa265d66b17e5ded
LazyKotaa <mylesmarshall06@gmail.com>
2024-12-13 19:00:31 -0700

    changed onclick for modal to image on product

--- src/components/GroceryList.jsx
+++ src/components/GroceryList.jsx
@@ -16,8 +16,8 @@ export default function GroceryList({ items, setModalData }) {
   return (
     <div className={styles.list}>
       {items.map(i => (
-        <div onClick={() => {setModalData(i)}} key={i.id} className={styles.item}>
-          <img src={"https://picsum.photos/seed/"+i.name+"/200/200.jpg"} alt={i.name} />
+        <div key={i.id} className={styles.item}>
+          <img onClick={() => {setModalData(i)}} src={"https://picsum.photos/seed/"+i.name+"/200/200.jpg"} alt={i.name} />
           <div className={styles.description}>
             <h1 className={styles.text}>${i.price}</h1>
             <h2 className={styles.text}>{i.name}</h2>
